4e524911b6a9e8eff50794320218b77d6e31f534
Euller Braz <euller.braz99@gmail.com>
2022-02-15 20:47:59 -0300

    feat: Created error middleware

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const { json } = require('body-parser');
 
 const TaskRouter = require('./app/routers/Task');
+const error = require('./app/middlewares/error');
 
 const app = express();
 
 app.use(json());
 
 app.use('/task', TaskRouter);
+
+app.use(error);
 
 module.exports = app;
