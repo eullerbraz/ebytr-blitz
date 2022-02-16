d69b419b26dbaa9ebb69ecda64ad14ff2352aae2
Euller Braz <euller.braz99@gmail.com>
2022-02-16 20:24:03 -0300

    feat: Created update task page

--- front-end/src/pages/RoutesPage.js
+++ front-end/src/pages/RoutesPage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import CreateTask from './CreateTask';
 import Tasks from './Tasks';
+import UpdateTask from './UpdateTask';
 
 const RoutesPage = () => {
   return (
     <Routes>
       <Route path="/" element={ <Tasks /> } />
       <Route path="/create" element={ <CreateTask /> } />
+      <Route path="/edit/:id" element={ <UpdateTask /> } />
     </Routes>
   );
 }
