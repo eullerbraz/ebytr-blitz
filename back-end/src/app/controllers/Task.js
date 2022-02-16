0a262a2b4293ff76790b440ec76fce6092697f48
Euller Braz <euller.braz99@gmail.com>
2022-02-15 21:14:40 -0300

    feat: Developed update and delete tasks endpoints

--- back-end/src/app/controllers/Task.js
+++ back-end/src/app/controllers/Task.js
@@ -14,7 +14,27 @@ const create = async (req, res, next) => {
   return res.status(201).json(task);
 }
 
+const update = async (req, res, next) => {
+  const { id } = req.params;
+  const {code, message, task} = await TaskService.update(id, req.body);
+
+  if (message) return next({ message, code });
+
+  return res.status(200).json(task);
+}
+
+const remove = async (req, res, next) => {
+  const { id } = req.params;
+  const { task, message, code } = await TaskService.remove(id);
+
+  if (message) return next({ message, code });
+
+  return res.status(200).json(task);
+}
+
 module.exports = {
   getAll,
   create,
+  update,
+  remove,
 }
--- back-end/src/app/services/Task.js
+++ back-end/src/app/services/Task.js
@@ -27,7 +27,32 @@ const create = async (task) => {
   return { task: created }
 }
 
+const update = async (id, task) => {
+  const { error } = validateTask(task);
+
+  if (error) {
+    const [{ message }] = error.details;
+    return { message, code: 400 };
+  }
+
+  const updated = await TaskModel.findByIdAndUpdate(id, task);
+
+  if (!updated) return { message: 'Invalid id', code: 400 }
+
+  return { task: updated }
+}
+
+const remove = async (id) => {
+  const deleted = await TaskModel.findByIdAndDelete(id);
+
+  if (!deleted) return { message: 'Invalid id', code: 400 }
+
+  return { task: deleted }
+}
+
 module.exports = {
   getAll,
   create,
+  update,
+  remove,
 }
