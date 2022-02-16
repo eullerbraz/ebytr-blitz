2c6416cbb7c6dce50d691d67d6d5d79c9a40c0ed
Euller Braz <euller.braz99@gmail.com>
2022-02-16 19:44:43 -0300

    feat: Create taks create page

---
+++ front-end/src/pages/CreateTask.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const CreateTask = () => {
+  const navigate = useNavigate();
+
+  const [state, setState] = useState({
+    name: '',
+    status: 'Pendente',
+  });
+
+  const handleChange = ({ target: { name, value } }) => {
+    setState({
+      ...state,
+      [name]: value,
+    })
+  }
+
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    const { name, status } = state;
+
+    const task = {
+      name,
+      status,
+      createDate: new Date(),
+    }
+
+    fetch(
+      'http://localhost:3001/task',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(task),
+      }
+    )
+
+    navigate(`/`)
+  }
+
+  return (
+    <form onSubmit={ handleClick }>
+      <input
+        type='text'
+        name='name'
+        placeholder='Digite um nova tarefa aqui'
+        onChange={ handleChange }
+      />
+      <select
+        name='status'
+        onChange={ handleChange }
+      >
+        <option value="Pendente">Pendente</option>
+        <option value="Em andamento">Em andamento</option>
+        <option value="Pronto">Pronto</option>
+      </select>
+      <button type='submit'>Criar farefa</button>
+    </form>
+  );
+}
+
+export default CreateTask;
